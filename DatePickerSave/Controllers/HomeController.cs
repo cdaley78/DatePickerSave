46f326e4e024a42775f6144fc0295ceb60d9db00
cjdaley@gmail.com <cjdaley@gmail.com>
2022-11-15 19:30:35 -0500

    Add project files.

---
+++ DatePickerSave/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using DatePickerSave.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatePickerSave.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CheckDatePicker();
+
+            return View();
+        }
+
+        public IActionResult Other()
+        {
+            CheckDatePicker();
+
+            return View();
+        }
+
+        private void CheckDatePicker()
+        {
+            if (!TempData.ContainsKey("datepickerval"))
+            {
+                TempData["datepickerval"] = SetDateToString(DateTime.Now);
+            }
+            else
+            {
+                TempData["datepickerval"] = SetDateToString(DateTime.Parse(TempData["datepickerval"].ToString()));
+                TempData.Keep("datepickerval");
+            }
+        }
+
+        private string SetDateToString(DateTime date)
+        {
+            return date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateDate([FromBody] DateModel model)
+        {
+            TempData["datepickerval"] = SetDateToString(DateTime.Parse(model.date));
+            TempData.Keep("datepickerval");
+
+            return Json(null);
+        }
+    }
+
+    public class DateModel
+    {
+        public string date { get; set; }
+    }
+}
